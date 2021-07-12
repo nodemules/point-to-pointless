f222357e518185cb08575f3b68a5589014de1e41
brenthaertlein <brenthaertlein@gmail.com>
2021-07-12 01:30:42 -0400

    update .gitignore, fix test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders header title', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const target = screen.getByText(/Point to Pointless/);
+  expect(target).toBeInTheDocument();
+  expect(target).toBeInstanceOf(HTMLHeadingElement)
 });
